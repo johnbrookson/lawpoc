9bee74060c279643557b2d645f83e2ca1f8e3a94
João Ribeiro Junior <joao.junior@engdb.com.br>
2024-11-18 22:03:18 -0300

    chore: try hello #2

--- api/index.go
+++ api/index.go
@@ -1,23 +1,10 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Processar o webhook
-	if r.Method != http.MethodPost {
-		http.Error(w, "Método não suportado", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var payload map[string]interface{}
-	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
-		http.Error(w, "Erro ao decodificar JSON", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintf(w, "Webhook recebido: %v", payload)
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
